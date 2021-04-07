65d90cecf1dbf23f8c6e0ba2073dbb182004f8d5
Aditya Verma <adityaverma@Adityas-MacBook-Air.local>
2021-04-07 11:42:10 +0530

    Intial Commit

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+//jshint esversion:6
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://admin-aditya:test123@cluster0.hmlzu.mongodb.net/todolistDB", {
+    useNewUrlParser: true
+});
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+const Item = mongoose.model("Item", itemsSchema);
+
+
+const item2 =new Item({
+    name: "Welcome to Todo-List"
+});
+
+const defaultArray=[item2];
 
 const app = express();
-var a=[];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
@@ -10,8 +28,12 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 app.post("/", (req, res) => {
-    var item = req.body.newTask;
-    a.push(item);
+    const itemName = req.body.newTask;
+    const item= new Item({
+        name :itemName
+    })
+    item.save();
+
     res.redirect('/');
 
 });
@@ -19,6 +41,7 @@ app.post("/", (req, res) => {
 
 app.get("/", (req, res) => {
 
+    
     var today = new Date();
     var option = {
         weekday: 'long',
@@ -26,12 +49,44 @@ app.get("/", (req, res) => {
         month: 'long'
     };
     var day = today.toLocaleDateString('en-US', option);
-
-    res.render("list", {
-        kindOfDay: day,
-        newTask: a
-
+    Item.find(function(err,foundItems){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundItems.length===0){
+                Item.insertMany(defaultArray,function(err){
+                    if(err){
+                        console.log(err);
+                    }
+                    else{
+                        console.log("Added to DataBase");
+                    }
+                })
+                }
+            res.render("list", {
+                kindOfDay: day,
+                newTask: foundItems
+        
+            });
+        }
+    })
+});
+app.post('/delete',(req,res)=>{
+    const delId = req.body.checkbox;
+    Item.deleteOne({_id : delId},function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("Deleted");
+            res.redirect('/');
+        }
     });
+
+
+    
+
 
 
 });
